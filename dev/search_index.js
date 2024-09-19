f123bc8a8d4dc46cee968c5b6acffcd71a71dde4
Documenter.jl <documenter@juliadocs.github.io>
2024-09-19 12:59:14 +0000

    build based on f849099

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MakiePublicationTheme","category":"page"},{"location":"#MakiePublicationTheme","page":"Home","title":"MakiePublicationTheme","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MakiePublicationTheme.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MakiePublicationTheme]","category":"page"}]
+}
